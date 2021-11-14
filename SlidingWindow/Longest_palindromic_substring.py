ac2a1381984460516a8acc6276b82eb27e132f3d
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2021-11-14 08:49:48 +0300

    Update Longest_palindromic_substring.py

--- SlidingWindow/Longest_palindromic_substring.py
+++ SlidingWindow/Longest_palindromic_substring.py
@@ -1,3 +1,27 @@
+"""
+Given a string s, return the longest palindromic substring in s.
+
+ 
+
+Example 1:
+
+Input: s = "babad"
+Output: "bab"
+Note: "aba" is also a valid answer.
+Example 2:
+
+Input: s = "cbbd"
+Output: "bb"
+Example 3:
+
+Input: s = "a"
+Output: "a"
+Example 4:
+
+Input: s = "ac"
+Output: "a"
+
+"""
 class Solution:
   def longestPalindrome(self, s: str) -> str:
     def helper(l,r):
@@ -15,5 +39,5 @@ class Solution:
       if len(test) > len(res):
         res = test
         
-      return res
+    return res
         
