8d25d9b546882802f65d9b95c6ee9366b3971b9d
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2021-11-19 13:59:55 +0300

    Create single-number-iii.py

---
+++ Arrays/bit-manipulation/single-number-iii.py
@@ -0,0 +1,42 @@
+"""
+Given an integer array nums, in which exactly two elements appear only once and all the other elements appear exactly twice. Find the two elements that appear only once. You can return the answer in any order.
+
+You must write an algorithm that runs in linear runtime complexity and uses only constant extra space.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,1,3,2,5]
+Output: [3,5]
+Explanation:  [5, 3] is also a valid answer.
+Example 2:
+
+Input: nums = [-1,0]
+Output: [-1,0]
+Example 3:
+
+Input: nums = [0,1]
+Output: [1,0]
+ 
+
+Constraints:
+
+2 <= nums.length <= 3 * 104
+-231 <= nums[i] <= 231 - 1
+Each integer in nums will appear twice, only two integers will appear once.
+"""
+
+class Solution:
+  def singleNumber(self, nums: List[int]) -> List[int]:
+     xor = 0 
+        for n in nums :
+            xor ^= n
+            
+        firstbit = xor & (xor - 1)^xor
+        num1 = 0
+        for n in nums:
+            if n & firstbit:
+                num1 ^= n
+                
+        return [num1, num1^xor]
