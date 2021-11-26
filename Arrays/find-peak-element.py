dac5bffbff82f214dd10f9134240928d290b5f33
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2021-11-26 08:42:52 +0300

    Update find-peak-element.py

--- Arrays/find-peak-element.py
+++ Arrays/find-peak-element.py
@@ -34,3 +34,10 @@ class Solution:
     maxindex = nums.index(maxval)
     
     return maxindex
+   
+   #leetcode solution
+   for i in range(len(nums)-1):
+            if nums[i] > nums[i+1]:
+                return i
+        return len(nums) - 1
+        
