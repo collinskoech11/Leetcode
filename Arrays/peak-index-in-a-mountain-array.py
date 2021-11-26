dbafb99929fa985d3a127279c3f591e88c45470a
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2021-11-26 08:30:32 +0300

    Update peak-index-in-a-mountain-array.py

--- Arrays/peak-index-in-a-mountain-array.py
+++ Arrays/peak-index-in-a-mountain-array.py
@@ -46,3 +46,8 @@ class Solution:
     maxarr = arr.index(maxval)
     
     return maxarr
+
+   """leetcode solution """
+    for i in range(len(arr)):
+            if arr[i]> arr[i+1]:
+                return i
