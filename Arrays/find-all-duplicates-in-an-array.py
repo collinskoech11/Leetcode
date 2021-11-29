3258f14abee21532c185db7be5590ad3fd2f635f
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2021-11-29 10:05:55 +0300

    Update find-all-duplicates-in-an-array.py

--- Arrays/find-all-duplicates-in-an-array.py
+++ Arrays/find-all-duplicates-in-an-array.py
@@ -31,3 +31,17 @@ Each element in nums appears once or twice.
 class Solution:
     def findDuplicates(self, nums: List[int]) -> List[int]:
         return [elem for elem, count in Counter(nums).items() if count == 2]
+   
+   
+   
+   
+   
+   #solution using set 
+   
+        seen = set()
+                seen_add = seen.add
+                # adds all elements it doesn't know yet to seen and all other to seen_twice
+                seen_twice = set( x for x in nums if x in seen or seen_add(x) )
+                # turn the set into a list (as requested)
+                return list( seen_twice )
+
