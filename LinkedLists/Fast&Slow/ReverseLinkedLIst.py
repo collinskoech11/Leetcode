196164a46cb453ebe15b0973ba831908c6bf6d74
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2021-11-30 18:50:15 +0300

    Update ReverseLinkedLIst.py

--- LinkedLists/Fast&Slow/ReverseLinkedLIst.py
+++ LinkedLists/Fast&Slow/ReverseLinkedLIst.py
@@ -24,6 +24,13 @@ Constraints:
 The number of nodes in the list is the range [0, 5000].
 -5000 <= Node.val <= 5000
 """
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+     
+     
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         prev, curr = None, head
