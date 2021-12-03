280320626df538a7cf2ff340044c8d2903bb422a
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2021-12-03 11:56:58 +0300

    Update Missing_number.py

--- Arrays/Missing_number.py
+++ Arrays/Missing_number.py
@@ -40,3 +40,7 @@ class Solution:
         for i in range(len(nums)):
             res += (i - nums[i])
         return res
+# Solution 2
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        return len(nums)*(len(nums)+1)//2- sum(nums)
