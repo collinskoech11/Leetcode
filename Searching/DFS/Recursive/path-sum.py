836ae93ba61b9fed5471daf1190ba4ce2228ad15
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2021-12-04 13:59:20 +0300

    Update path-sum.py

--- Searching/DFS/Recursive/path-sum.py
+++ Searching/DFS/Recursive/path-sum.py
@@ -18,3 +18,7 @@ class Solution:
             return (dfs(node.left, curSum) or dfs(node.right, curSum))
 
         return dfs(root, 0)
+#start with targetSum then subtract the value of the node from the targetSum return True if the difference is 0
+class SolutionTwo:
+    def hasPathSum(self, root:OPtional[TreeNode], targetSum: int) -> bool:
+        
