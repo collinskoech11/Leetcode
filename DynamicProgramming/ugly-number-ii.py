9b991adafbad983023529a62da5d53665664acb3
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2021-12-16 10:40:10 +0300

    Update ugly-number-ii.py

--- DynamicProgramming/ugly-number-ii.py
+++ DynamicProgramming/ugly-number-ii.py
@@ -10,9 +10,17 @@
         
         
 the function returns the nth ugly number
-"""
 
-class Solution:
+"""
+while len(ugly) < n:
+            while ugly[two]*2 <= ugly[-1]:
+                two += 1
+            while ugly[three]*3 <= ugly[-1]:
+                three+=1
+            while ugly[five]*5 <= ugly[-1]:
+                five+=1
+            ugly.append(min(ugly[two]*2, ugly[three]*3, ugly[five]*5))
+"""class Solution:
     def nthUglyNumber(self, n: int) -> int:
         ugly = [1]
         two = three = five = 0
@@ -25,6 +33,25 @@ class Solution:
                 five+=1
             ugly.append(min(ugly[two]*2, ugly[three]*3, ugly[five]*5))
             
-        return ugly[-1]
+        return ugly[-1]"""
+class Solution:
+    def nthUglyNumber(self, n:int):
+        nums = [1]
+        idx_2, idx_3, idx_5 = 0,0,0
+        for i in range(n-1):
+            next2 = nums[idx_2]*2
+            next3 = nums[idx_3]*3
+            next5 = nums[idx_5]*5
+            next=min(next2,next3,next5)
+            nums.append(next)
+            if next==next2:
+                idx_2+=1
+            if next == next3:
+                idx_3+=1
+            if next == next5:
+                idx_5+=1
+        return nums[-1]
+            
+    
     
     
