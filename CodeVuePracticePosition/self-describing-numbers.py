fed87378c0ff2b58aa57ea1d17e0feaf76032463
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2021-12-18 19:59:26 +0300

    Create self-describing-numbers.py

---
+++ CodeVuePracticePosition/self-describing-numbers.py
@@ -0,0 +1,12 @@
+"""
+Self Describing Numbers
+Programming challenge description:
+A number is a self-describing number when (assuming digit positions are labeled 0 to N-1), the digit in each position is equal to the number of times that that digit appears in the number.
+Input:
+Your program should read lines of text from standard input. Each line contains a single positive integer, N.
+Output:
+For each input N, print 1 to standard output if N is a self-describing number. Otherwise, print 0.
+
+For the curious, here''s how 2020 is a self-describing number: Position 0 has value 2 and there are two 0s in the number. Position 1 has value 0 because there are no 1's in the number. Position 2 has value 2 and there are two 2's. And the position 3 has value 0 and there are zero 3's.
+Test 1
+"""
