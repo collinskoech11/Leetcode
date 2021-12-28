3d47c814d9f88e4e519612611ff1fc9073852546
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2021-12-28 19:02:09 +0300

    Update self-describing-numbers.py

--- CodeVuePracticePosition/self-describing-numbers.py
+++ CodeVuePracticePosition/self-describing-numbers.py
@@ -10,3 +10,21 @@ For each input N, print 1 to standard output if N is a self-describing number. O
 For the curious, here''s how 2020 is a self-describing number: Position 0 has value 2 and there are two 0s in the number. Position 1 has value 0 because there are no 1's in the number. Position 2 has value 2 and there are two 2's. And the position 3 has value 0 and there are zero 3's.
 Test 1
 """
+def isSElfDescriptiveNumber(num):
+    s = str(num)
+    for i in range(len(s)):
+        temp_char = s[i]
+        b = ord(temp_char) - ord('0')
+        count = 0
+        for j in range(len(s)):
+            temp = ord(s[j]) - ord('0')
+            if (temp == i):
+                count += 1
+         if (count != b):
+            return False
+    return True
+
+if __name__=="__main__":
+    for i in range(100000000):
+        if (isSelfDescriptiveNumber(i)):
+            print(i)
