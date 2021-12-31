7569e2d92e3b432d4d915200e80e4d6e52f0cc2b
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2021-12-31 11:28:39 +0300

    Create linked-list-cycle-ii.py

---
+++ LinkedLists/Fast&Slow/linked-list-cycle-ii.py
@@ -0,0 +1,18 @@
+#Definition of a singly linked list.
+#class ListNode:
+#   def __init__(self, x):
+#       self.val = x
+#       self.next = None
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = fast  = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                node = head
+                while node != slow:
+                    node = node.next
+                    slow = slow.next
+                return node
+        return None 
