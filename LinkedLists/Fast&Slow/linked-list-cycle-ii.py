43f508bcd2211a5ed78d08338481af83aa8e7f48
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2021-12-31 11:34:43 +0300

    Update linked-list-cycle-ii.py

--- LinkedLists/Fast&Slow/linked-list-cycle-ii.py
+++ LinkedLists/Fast&Slow/linked-list-cycle-ii.py
@@ -1,3 +1,33 @@
+""" given the head of a singly linked list determine if it has a cycle and retrn the position where the cycle occurs
+Example 1:
+
+
+Input: head = [3,2,0,-4], pos = 1
+Output: tail connects to node index 1
+Explanation: There is a cycle in the linked list, where tail connects to the second node.
+Example 2:
+
+
+Input: head = [1,2], pos = 0
+Output: tail connects to node index 0
+Explanation: There is a cycle in the linked list, where tail connects to the first node.
+Example 3:
+
+
+Input: head = [1], pos = -1
+Output: no cycle
+Explanation: There is no cycle in the linked list.
+ 
+
+Constraints:
+
+The number of the nodes in the list is in the range [0, 104].
+-105 <= Node.val <= 105
+pos is -1 or a valid index in the linked-list.
+
+
+"""
+
 #Definition of a singly linked list.
 #class ListNode:
 #   def __init__(self, x):
