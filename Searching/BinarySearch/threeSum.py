686c01b35b2ca2e1a600dd32f625e44ba228b0e7
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2022-01-05 11:43:08 +0300

    Update threeSum.py

--- Searching/BinarySearch/threeSum.py
+++ Searching/BinarySearch/threeSum.py
@@ -1,3 +1,30 @@
+"""
+Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+Notice that the solution set must not contain duplicate triplets.
+
+ 
+
+Example 1:
+
+Input: nums = [-1,0,1,2,-1,-4]
+Output: [[-1,-1,2],[-1,0,1]]
+Example 2:
+
+Input: nums = []
+Output: []
+Example 3:
+
+Input: nums = [0]
+Output: []
+ 
+
+Constraints:
+
+0 <= nums.length <= 3000
+-105 <= nums[i] <= 105
+
+"""
 class Solution(object):
     def threeSum(self, nums):
         """
@@ -7,7 +34,7 @@ class Solution(object):
         nums.sort()
         res = []
         target = 0
-        if len(nums) <= 1:
+        if len(nums) <= 1:# Base case
             return []
         else:
             for i in range(len(nums)-2):
