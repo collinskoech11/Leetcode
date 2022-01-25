23fca631a53efd12190337600a19ba6842bf9fba
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2022-01-25 12:38:36 +0300

    Update remove-element.py

--- Arrays/remove-element.py
+++ Arrays/remove-element.py
@@ -4,23 +4,26 @@
 
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        if len(nums) == 1:
+        if len(nums) == 1:# check the length of the array
             if nums[0] == val:
-                return 0
+                return 0# if first element = va return false
             else:
-                return 1
-        start = 0
-        last = len(nums)-1
-        result = 0
-        while start <= last:
-            if nums[start] == val:
-                if nums[last] == val:
-                    last -= 1
-                else:
+                return 1# return true
+        
+        last, start =  len(nums)-1, 0# Binary Search Pointers
+        while start <= last:# while start is less than last
+            if nums[start] == val:# check if element on start pointer is equal to val
+                if nums[last] == val:# check if element on last pointer is equal to val
+                    last -= 1# shift last pointer one step towards the start
+                    print(nums,start)
+                else:# while start is not less than last
                     nums[start] = nums[last]
                     last -= 1
                     start += 1
+                    # print(nums,start)
             else:
                 start += 1
+                print(nums,start)
         return start
+        print(nums,start)
         
