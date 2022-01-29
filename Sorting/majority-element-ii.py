a1ba951b903f2a0972fb514cad5c6f94b65ba3d6
collinskoech11 <collinskoechck34@gmail.com>
2022-01-29 11:23:54 +0300

    solutions refresher

--- Sorting/majority-element-ii.py
+++
@@ -1,7 +0,0 @@
-"""given an array nums return the elements that appears n/3 or more times
-example nums = [3,2,3]
-returns [3]"""
-
-class Solution:
-    def majorityElement(self, nums:List[int]) -> List[int]:
-        return [elem for elem, count in Counter(nums).items() if count > len(nums)/3]
