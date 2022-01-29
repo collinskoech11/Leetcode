9116f24a09d6bf9e447828da42d246b91ad8c292
collinskoech11 <collinskoechck34@gmail.com>
2022-01-29 11:16:39 +0300

    solutions refresher

--- stringManipulation/detect-capital.py
+++
@@ -1,8 +0,0 @@
-# Given a string return true if the usage of capitals is correct
-# word = "USA"
-# return True
-# word = "FLaG"
-# return Flase
-class Solution:
-    def detectCapitalUse(self, word: str) -> bool:
-        return word.isupper() or word.islower() or word.istitle
