727e02210f3f9c638f89ed939ec0a03be2db71f0
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2022-02-08 11:04:32 +0300

    Update Missing-number.py

--- Arrays/Missing-number.py
+++ Arrays/Missing-number.py
@@ -1,3 +1,14 @@
+"""
+Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+
+ 
+
+Example 1:
+
+Input: nums = [3,0,1]
+Output: 2
+Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
+"""
 class Solution:
     # solution using a single for loop O(N) runtime O(1) space complexity -> we only allocate space for res once 
     def missingNUmber(self, nums:List[int]) -> int:
