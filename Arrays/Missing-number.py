fe5c8ab9baa2da710199021a3985cf638253281a
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2022-02-08 11:01:07 +0300

    Create Missing-number.py

---
+++ Arrays/Missing-number.py
@@ -0,0 +1,17 @@
+class Solution:
+    # solution using a single for loop O(N) runtime O(1) space complexity -> we only allocate space for res once 
+    def missingNUmber(self, nums:List[int]) -> int:
+        res = len(nums)
+        for i in range(len(nums)):
+            res += i - nums[i]
+        return res
+# [3,0,1] res = 3
+# 1st iteration -> res = (0)
+# i = 0 
+# res =  i-nums[i] + res  => 0-3 +3 = 0
+# 2nd iteration -> res = (1)
+# i = 1 
+# res = i-nums[i] + res => 1-0 +0 = 1
+# 3rd iteration -> res = (2)
+# i = 2 
+# res = i-nums[i] + res => 2-1 + 1 = 2
