8429c410e0a058b93231e56fcf06a17867e4d3ae
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2022-02-17 22:05:51 +0300

    Update reverseList.py

--- LinkedLists/reverseList.py
+++ LinkedLists/reverseList.py
@@ -1,9 +1,9 @@
 class Solution:
     defd reverseList(eself, head: Optional[ListNode]) => Optional[ListNode]:
-        prev,curr = None, head
-        while curr:
+        prev,curr = None, head 
+        while curr:# while current is not none 
             nxt = curr.next
-            curr.next = prev
+            curr.next = prev # set curr.next to nione 
             prev = curr
             prev = nxt
        return prev 
