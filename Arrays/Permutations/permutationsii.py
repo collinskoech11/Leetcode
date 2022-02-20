17c336e1a54a3518903d943c3562f4e5e9e22f6e
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2022-02-21 02:37:34 +0300

    Update permutationsii.py
    still not completely comprehended

--- Arrays/Permutations/permutationsii.py
+++ Arrays/Permutations/permutationsii.py
@@ -1,5 +1,5 @@
 """
-Given a collection of numbers nums, that might contain duplicates return all ossible permutations in any order
+Given a collection of numbers nums, that might contain duplicates return all possible permutations in any order
 Example 1:
 Input: [1,1,2]
 Output: [[1,1,2],[1,2,1],[2,1,1]]
@@ -12,16 +12,18 @@ class Solution:
     def permuteUnique(elf, nums:List[int]) -> List[List[int]]:
         res = []# store the list of all permutations 
         perm = []# store each urrent permutation 
-        count = Counter(nums)# use counter to check for duplicates 
+        count = Counter(nums)# use counter to check for duplicates from d.collections 
+        # || { n:0 for n in nums } set n to zero for each unique element in nums
+        # for n in nums: count[n] += 1 for every element update its count by one
         def dfs():# Backtracking fx
-            if len(perm) == len(perm):# Base case 
-                res.append(perm.copy())
+            if len(perm) == len(nums):# Base case if the permutation is complete and we dont have any number to add
+                res.append(perm.copy())# 
                 return
-            for i in count:
-                if count[n] > 0:
-                    perm.append(n)
-                    count[n] -= 1
-                    dfs()
+            for i in count:#brute force => choose every choice that we can / go through every number in count (every key is unique
+                if count[n] > 0:# make sure count is greater than 0 meaning that we are alowed to choose it
+                    perm.append(n)# append the current n to the permutration
+                    count[n] -= 1# decrement the current count by one
+                    dfs()# call the dfs() fx at this point we have gotten to the base case
                     count[n] += 1
                     perm.pop()
         dfs()
