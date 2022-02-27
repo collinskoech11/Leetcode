34e56e4559f661d8a4b1b9f3bd0292013f166b1a
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2022-02-28 00:56:15 +0300

    Update and rename Backtrackin/letterCombinations.py to Backtracking/letterCombinations.py

--- Backtracking/letterCombinations.py
+++ Backtracking/letterCombinations.py
@@ -8,6 +8,16 @@ Example 1:
 Input: digits = "23"
 Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
 
+Example 2:
+
+Input: digits = ""
+Output: []
+
+
+Example 3:
+
+Input: digits = "2"
+Output: ["a","b","c"]
 """
 
 
