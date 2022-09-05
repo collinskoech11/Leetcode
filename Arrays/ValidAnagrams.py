2eca5b9c2d6bbfb6d75fce92365c13fce543462b
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2022-09-05 13:13:38 +0300

    Update ValidAnagrams.py

--- Arrays/ValidAnagrams.py
+++ Arrays/ValidAnagrams.py
@@ -24,13 +24,16 @@ s and t consist of lowercase English letters.
 """
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        scounter = {}
+        if len(s) !== len(t): # check lengths of both strings
+            return False # valid anagrams have equal lengths
+         
+        scounter = {}# create a dictionary for both
         tcounter = {}
         for i in s:
             if i not in scounter:
-                scounter[i] = 1
+                scounter[i] = 1#initialize counter as one if not already in dictionary
             else:
-                scounter[i] += 1
+                scounter[i] += 1# increment value if element is already present
         for j in t:
             if j not in tcounter:
                 tcounter[j] = 1
@@ -39,16 +42,16 @@ class Solution:
         print(scounter, tcounter)
         for r in scounter:
             if r in tcounter:
-                if scounter[r] != tcounter[r]:
+                if scounter[r] != tcounter[r]: # compare valur in sCounter and value in tCounter
                     return False
             else: 
                 return False
         for x in tcounter:
             if x in scounter:
-                if tcounter[x] != scounter[x]:
+                if tcounter[x] != scounter[x]: # compare valur in sCounter and value in tCounter
                     return False
             else:
                 return False
-        return True
+        return True # return True if loop completes
         
             
